0198b2c7adef6abb60bfb6450dc2d4cd63af026c
NoorFatimaCS <noorfatimallu721@gmail.com>
2025-08-16 23:49:15 +0500

    voting_eligibility.py

---
+++ voting_eligibility.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+print("Eligible to vote")
+else:
+print("Not eligible to vote")
+
+Output 
+Enter your age: 20
+Eligible to vote
+
+Enter your age: 15
+Not eligible to vote
