13a731bbfbee1bdb5e6084b3e3e74466f9a8444a
NoorFatimaCS <noorfatimallu721@gmail.com>
2025-08-23 09:51:10 +0500

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+Program: Checking Python Data Types
+
+x = 10          # Integer
+y = 2.53        # Float
+name = "noor"   # String
+coine = True    # Boolean
+
+# Print the data types of each variable
+print(type(x), type(y), type(name), type(coine))
+
